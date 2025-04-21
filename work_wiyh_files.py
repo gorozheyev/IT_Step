746c41196470642423abc87344ac959c1765887f
gorozheyev <dgorozheyev@gmail.com>
2025-04-22 00:33:32 +0200

    work with file

--- work_wiyh_files.py
+++ work_wiyh_files.py
@@ -66,6 +66,20 @@
 #     content = file.read()
 #     print(content)
 
+from datetime import datetime, date
+
+
+def is_valid_date(date_str: str, date_format: str = "%d.%m.%Y") -> bool:
+    """
+    Проверяет, соответствует ли строка date_str формату date_format.
+    По умолчанию формат "%d.%m.%Y" (день.месяц.год).
+    """
+    try:
+        datetime.strptime(date_str, date_format)
+        return True
+    except ValueError:
+        return False
+
 
 print("Ваши задачи на день")
 
@@ -74,8 +88,9 @@ while True:
     action = input("Выберете опцию для дальнейшего действия:\n"
                    "1 - добавить задачу\n"
                    "2 - просмотреть задачи\n"
+                   "3 - введите задачу с дедлайном\n"
                    "0 - выйти из приложения\n"
-                   "- ")
+                   "Ваш выбор: ")
 
     if action == "0":
         print("Программа завершена")
@@ -83,7 +98,7 @@ while True:
 
     if action == "1":
         with open("tasks.txt", "a") as file:
-            new_task = input("Введите задачу:\n")
+            new_task = input("\nВведите задачу:\n")
             file.write(new_task + "\n")
             print("Задача добавлена в список\n")
 
@@ -91,7 +106,7 @@ while True:
         try:
             with open("tasks.txt", "r") as file:
                 tasks = file.readline()
-                print("Список ваших задач:")
+                print("\nСписок ваших задач:")
                 while tasks:
                     print(tasks, end="")
                     tasks = file.readline()
@@ -99,6 +114,28 @@ while True:
         except FileNotFoundError:
             print("Список задач пуст")
 
+    elif action == "3":
+        with open("tasks.txt", "a") as file:
+            new_task = input("\nВведите задачу:\n")
+            file.write(new_task + " ")
+            dedline = input("введите дедлайн в формате ДД-ММ-ГГГГ: ")
+            if is_valid_date(dedline):
+                file.write(dedline + " ")
+                done = int(input("Выберите статус:\n"
+                             "1 - выполнена\n"
+                             "0 - не выполнена\n"
+                             ": "))
+                if done == 1:
+                    file.write("[Task completed]\n")
+                    print("Задача с дедлайном добавлена в список\n")
+                elif done == 0:
+                    file.write("[Task is not completed]\n")
+                    print("Задача с дедлайном добавлена в список\n")
+                else:
+                    print("Выберите статус 1 или 2")
+
+            else:
+                print("Введите дату в правильном формате")
 
     else:
         print("Неверный ввод выберите 0, 1 или 2")
